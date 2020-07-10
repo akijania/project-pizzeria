40200bca02f612eec967a9936669e3cafbccce33
Anna Kijania <kijania.anna@gmail.com>
2020-07-10 11:55:26 +0200

    Add pages activation

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,45 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[0].id;
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+
+    for(const link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        const id = clickedElement.getAttribute('href').replace('#','');
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
+
+  },
+  activatePage(pageId){
+    const thisApp = this;
+    for(const page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    for(const link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId);
+    }
+
+  },
   initMenu: function(){
     const thisApp = this;
     // console.log('thisApp.data', thisApp.data);
@@ -38,6 +75,12 @@ const app = {
     console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
+  initBooking: function(){
+    const thisApp = this;
+    const bookingElem = document.querySelector(select.containerOf.booking);
+    thisApp.booking =new Booking(bookingElem);
+  },
+
   init: function(){
     const thisApp = this;
     // console.log('*** App starting ***');
@@ -46,8 +89,9 @@ const app = {
     // console.log('settings:', settings);
     // console.log('templates:', templates);
     thisApp.initData();
-      
+    thisApp.initPages();  
     thisApp.initCart();
+    thisApp.initBooking();
   },
 
   
