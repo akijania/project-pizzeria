6785f2adfec563e429c4e36a20b5a5eaec8b7039
Anna Kijania <kijania.anna@gmail.com>
2020-07-13 23:16:31 +0200

    Add DatePicker and HourPicker

---
+++ src/js/components/DatePicker.js
@@ -0,0 +1,56 @@
+import BaseWidget from './BaseWidget.js';
+import {utils} from '../utils.js';
+import {select, settings} from '../settings.js';
+
+class DatePicker extends BaseWidget{
+  constructor(wrapper){
+    super(wrapper, utils.dateToStr(new Date()));
+    const thisWidget = this;
+
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
+    thisWidget.initPlugin();
+    
+        
+  }
+  initPlugin(){
+    const thisWidget = this;
+    thisWidget.minDate = new Date(thisWidget.value);
+    thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
+    console.log(thisWidget.minDate);
+    
+   
+    flatpickr(thisWidget.dom.input, {
+      'defaultDate': thisWidget.minDate,
+      'minDate': thisWidget.minDate,
+      'maxDate': thisWidget.maxDate,
+      'disable': [
+        function(date) {
+          // return true to disable
+          return (date.getDay() === 1);
+    
+        }
+      ],
+      'locale': {
+        'firstDayOfWeek': 1 // start week on Monday
+      },
+      onChange: function(minDate, dateStr) {
+        console.log(minDate);
+        console.log(dateStr);
+      },
+      
+     
+    });
+    
+
+  }
+  parseValue(newValue){
+    return newValue;
+  }
+  isValid(){
+    return true; 
+  }
+  renderValue(){    
+  }
+
+}
+export default DatePicker;
--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -16,7 +16,7 @@ class Product{
     thisProduct.initAmountWidget();
     thisProduct.processOrder();
 
-    console.log('new Product', thisProduct);
+    // console.log('new Product', thisProduct);
   }
 
   renderInMenu(){
@@ -115,7 +115,7 @@ class Product{
 
     /* set the contents of thisProduct.priceElem to be the value of variable price */
     thisProduct.priceElem.innerHTML = thisProduct.price;  
-    console.log(thisProduct.params);
+    // console.log(thisProduct.params);
 
   }
   initAmountWidget(){
